ef78f004b0448e9dd296d59080ac4b5410144aff
Hen443 <henula09106@gmail.com>
2019-10-11 17:55:57 +0400

    Revert "Added icon"
    This reverts commit de5df013620b496303f9b15faf50cc8ea9a3b02d.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
-var port = process.env.PORT || 2440
+var port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({
     extended: false
